589fe583a1e658a5ef1b0a2fd3cd10d0611afc56
michaelgwilliams774 <132536868+michaelgwilliams774@users.noreply.github.com>
2023-05-16 03:20:12 +0000

    Base knowledge of Prototype Inheritance

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ let lizard = {
 // const singLizard = dragon.sing.bind(lizard)
 // console.log(singLizard())
 
-lizard.__proto__ = dragon
-console.log(lizard.sing())
-console.log(lizard.fire)
+lizard.__proto__ = dragon  // Never use this in practice!
+// console.log(lizard.sing())
+// console.log(lizard.fire)
+console.log(dragon.isPrototypeOf(lizard))
+
+for (let prop in lizard) {
+  if (lizard.hasOwnProperty(prop)) {
+    console.log('lizard own property: ', prop)
+  } else {
+    console.log('prototype chain property: ', prop)  
+  }
+}
